de0fc363c2c15b194d2d0107483b1e79e24d727b
James Higgins and Trevor Fast <trevor@>
2017-04-19 14:56:53 -0700

    add second test spec and code to pass

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,7 +1,11 @@
 class String
   define_method(:beats?) do |second|
    if (self == "rock") and (second == "scissors")
+     puts "Smash!! Rock Wins"
      true
+   else (self == "rock") and (second == "paper")
+     puts "coverup!! Paper Wins"
+     false
    end
  end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -5,5 +5,7 @@ describe('') do
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".beats?("scissors")).to(eq(true))
     end
-
+    it("returns false if rock is the object and paper is the argument") do
+      expect("rock".beats?("paper")).to(eq(false))
+    end
   end
