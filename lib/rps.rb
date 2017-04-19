b85e6d39e8a6c77295aaf52b09182b211f2e6ff5
James Higgins and Trevor Fast <james@>
2017-04-19 15:12:40 -0700

    add test and spec for paper and scissor contest

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,14 +1,38 @@
 class String
   define_method(:beats?) do |second|
+    # rock vs --
    if (self == "rock") and (second == "scissors")
      puts "Smash!! Rock Wins"
      true
    elsif (self == "rock") and (second == "paper")
      puts "coverup!! Paper Wins"
      false
-   else (self == "rock") and (second == "rock")
+   elsif (self == "rock") and (second == "rock")
      puts "tie game play again"
      "tie game play again"
+
+
+    # paper vs --
+  elsif (self == "paper") and (second == "rock")
+    puts "coverup!! Paper Wins"
+    true
+  elsif (self == "paper") and (second == "scissors")
+     puts "snip!! scissors Wins"
+     false
+   elsif (self == "paper") and (second == "paper")
+     puts "tie game play again"
+     "tie game play again"
+
+     # scissors vs --
+   elsif (self == "scissors") and (second == "paper")
+     puts "snip!! scissors Wins"
+     true
+   elsif (self == "scissors") and (second == "rock")
+      puts  "Smash!! Rock Wins"
+      false
+    else (self == "scissors") and (second == "scissors")
+      puts "tie game play again"
+      "tie game play again"
    end
  end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -2,6 +2,7 @@ require('rspec')
 require('rps')
 
 describe('') do
+  # rock vs --
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".beats?("scissors")).to(eq(true))
     end
@@ -11,4 +12,28 @@ describe('') do
     it("returns 'tie game play again' if rock is the object and rock is the argument") do
       expect("rock".beats?("rock")).to(eq("tie game play again"))
     end
+
+    # paper vs --
+  it("returns true if paper is the object and rock is the argument") do
+    expect("paper".beats?("rock")).to(eq(true))
+    end
+    it("returns false if paper is the object and scissors is the argument") do
+      expect("paper".beats?("scissors")).to(eq(false))
+    end
+    it("returns 'tie game play again' if paper is the object and paper is the argument") do
+      expect("paper".beats?("paper")).to(eq("tie game play again"))
+    end
+    # scissors vs --
+    it("returns true if scissors is the object and paper is the argument") do
+      expect("scissors".beats?("paper")).to(eq(true))
+      end
+      it("returns false if scissors is the object and rock is the argument") do
+        expect("scissors".beats?("rock")).to(eq(false))
+      end
+      it("returns 'tie game play again' if scissors is the object and scissors is the argument") do
+        expect("scissors".beats?("scissors")).to(eq("tie game play again"))
+      end
+    # lizard vs --
+
+    # spock vs --
   end
