51ca572d0b0495256ceba41a7921341ab4e1729c
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-20 06:08:41 -0700

    add player two outcome for integration and erb files

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require('rspec')
+require('pry')
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/rps')
@@ -5,4 +7,11 @@ also_reload('./lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/outcome') do # these aren't accurate. Just place holders.
+  @decision1 = params.fetch('player1')
+  @decision2 = params.fetch('player2')
+  @outcome = ''.beats(@decision1,@decision2)
+  erb(:outcome)
 end
--- spec/rps_integration_spec.rb
+++ spec/rps_integration_spec.rb
@@ -1,1 +1,17 @@
 # gems that need to be included
+require('capybara/rspec')
+require('./app')
+require('launchy')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+# Integration test
+  describe('outcome', {:type => :feature}) do
+    it('processes the players entry and returns the winner of the competition') do
+      visit('/')
+      fill_in('player1', :with => 'rock')
+      fill_in('player2', :with => 'scissors')
+      click_button('Rochambeau')
+      expect(page).to have_content('Smash!! Rock Wins') # <=this is where the outcome of the test would be
+    end
+  end
